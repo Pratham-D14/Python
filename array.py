da408d0624bf1332787ddcf32a2cedf5894f41ec
pratham.darji <pratham.darji@deptagency.com>
2025-10-05 09:51:31 +0530

    practice methods of array

---
+++ array.py
@@ -0,0 +1,95 @@
+# Array and it's method in python
+arr = [1, 2, 3]
+
+# Adding and Removing Element from Array
+# Append methods add element at last of the existing array
+arr.append(5)
+print(arr)
+
+# Pop methods remove the last value from the existing array
+arr.pop()
+print(arr)
+
+# Inserting value at a particular index
+arr.insert(1, 6)  # args -> (index, value)
+print(arr)
+
+# Modifying elements using index
+arr[0] = 0
+print(arr)
+
+
+# initialize array of size n with default value of 1
+n = 5
+arr = [1] * n
+
+# Sublist  (slicing an array)
+arr = [1, 2, 3, 4]
+print(arr[1:3])  # -> From index 1-2 as 3 is excluded
+print(arr[0:4])
+print(arr[0:])
+print(arr[:4])
+
+
+# Unpacking
+a, b, c = [1, 2, 4]  # the no.of variables on LHS should be equal to RHS
+
+
+# Looping through Arrays
+nums = [1, 2, 3]
+
+# using for-loop/index
+for i in range(len(nums)):
+    print(nums[i])
+
+# Without index
+for n in nums:
+    print(n)
+
+
+# with index and value using enumerate function
+for i, n in enumerate(nums):
+    print(i, n)
+
+
+# Loop through multiple arrays simultaneously
+# with unpacking
+nums1 = [1, 2, 3]
+nums2 = [4, 5, 6]
+for n1, n2 in zip(nums1, nums2):
+    print(n1, n2)
+
+
+# Reversing an Array
+nums1.reverse()
+print(nums1)
+
+
+# Sorting of array
+# Ascending order
+nums1.sort()
+print(nums1)
+
+# Descending order
+nums1.sort(reverse=True)
+print(nums1)
+
+# Sorting a string
+arr = ['bob', 'alice', 'jane', 'doe']
+# Sorting alphabetically
+arr.sort()
+print(arr)
+
+# Custom Sort (by length of string)
+arr.sort(key=lambda x: len(x))
+print(arr)
+
+
+# List comprehension
+arr = [i+i for i in range(5)]
+print(arr)
+
+
+# 2D List
+arr = [[0] * 4 for i in range(4)]
+print(arr)
--- question.py
+++ question.py
@@ -3,12 +3,12 @@ users = ["u1", "u1", "u2", "u2", "u3", "u3", "u1"]
 timers = [1, 2, 4, 18, 4, 6, 20]
 
 store = {}
-session = {}  # <- use if you want to track each users session
+session = {}  # <- use if you want to track each user session
 totalCount = 0
 
 
 def countSession(timing: int, users, timers) -> None:
-    # global totalCount
+    global totalCount
     for i in range(len(users)):
         if users[i] not in store:
             store[users[i]] = timers[i]
